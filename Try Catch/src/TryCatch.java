65138cb0d60c7b632beecfa37a492cf26f09bb9b
thomas.bradac <thomas.bradac@MU-C1-10.mullen.pvt.k12.co.us>
2026-01-28 14:58:46 -0700

    Initial Commit

---
+++ Try Catch/src/TryCatch.java
@@ -0,0 +1,11 @@
+
+public class TryCatch
+	{
+
+		public static void main(String[] args)
+			{
+				// Make try catch statement. I want to make exception index out of bounds from an array.
+
+			}
+
+	}
