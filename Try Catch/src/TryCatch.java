334652d6ce3db0d1cedffe7a6766fae7d6d0b76a
Marc <Marc@MARC-LAPTOP>
2026-02-01 15:56:28 -0700

    Finished Try Catch Statement

--- Try Catch/src/TryCatch.java
+++ Try Catch/src/TryCatch.java
@@ -5,6 +5,19 @@ public class TryCatch
 		public static void main(String[] args)
 			{
 				// Make try catch statement. I want to make exception index out of bounds from an array.
+			
+			String names[] = new String[5];
+			
+			try
+			{
+				names[15] = "Mike McGuire";
+			}
+			
+			catch (ArrayIndexOutOfBoundsException exc)
+			{
+				System.out.println("Try again, that index is out of bounds for the given array.");
+			}
+			
 
 			}
 
